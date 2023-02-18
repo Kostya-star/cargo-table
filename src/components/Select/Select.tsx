60fe928ac0f6c86854064633cd6f4e7f4eb0d7d9
Constantin <konstantindanilov19@hotmail.com>
2023-02-18 11:07:59 +0200

    styled out the select

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,27 +1,75 @@
 import { FC } from 'react';
 import SelectComponent from 'react-select';
-import { SingleValue } from 'react-select/dist/declarations/src';
-import s from './Select.module.scss'
+import {
+  SingleValue,
+  StylesConfig,
+  MultiValue,
+} from 'react-select/dist/declarations/src';
+import s from './Select.module.scss';
+
 
 const options = [
   { value: 'From', label: 'From' },
   { value: 'To', label: 'To' },
 ];
 
-interface IOption {
+const classNames: StylesConfig<IOption> = {
+  control: (baseStyles) => ({
+    ...baseStyles,
+    fontSize: '14px',
+    transition: '.4s',
+    border: '0',
+    backgroundColor: '#363640',
+    '&:hover': {
+      backgroundColor: 'gray',
+      border: 'none',
+    },
+    '&:focus': {
+      background: '#F6F6F6',
+      boxShadow: '0px 0px 5px #D9D9D9',
+      border: 'none',
+    },
+    cursor: 'pointer',
+  }),
+  option: (baseStyles, state) => ({
+    ...baseStyles,
+    cursor: 'pointer',
+    backgroundColor: state.isSelected ? '#363640' : 'white',
+    transition: '0.2s',
+    '&:hover': {
+      backgroundColor: 'grey',
+      color: 'white',
+    },
+  }),
+  dropdownIndicator: (baseStyles) => ({
+    ...baseStyles,
+    color: 'white',
+  }),
+  clearIndicator: (baseStyles) => ({
+    ...baseStyles,
+    color: 'white',
+  }),
+  singleValue: (baseStyles) => ({
+    ...baseStyles,
+    color: 'white'
+  })
+};
+
+
+export interface IOption {
   value: string;
   label: string;
 }
 
 interface ISelectProps {
-  onChange: (option: string) => void;
+  onChange: (option: SingleValue<IOption>) => void;
 }
 
 export const Select: FC<ISelectProps> = ({ onChange }) => {
-  const onSelectChange = (option: SingleValue<IOption>) => {
-    if (option) {
-      onChange(option.value);
-    }
+  const onSelectChange = (
+    option: SingleValue<IOption> | MultiValue<IOption>,
+  ) => {
+    onChange(option as SingleValue<IOption>);
   };
 
   return (
@@ -30,7 +78,8 @@ export const Select: FC<ISelectProps> = ({ onChange }) => {
         options={options}
         onChange={onSelectChange}
         isClearable={true}
-        placeholder='Выбрать'
+        placeholder="Выбрать"
+        styles={classNames}
       />
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,19 +14,22 @@ import 'devextreme/dist/css/dx.material.blue.dark.css';
 import { Select } from 'components/Select/Select';
 import { useState } from 'react';
 import 'scss/all.scss';
+import { SingleValue } from 'react-select/dist/declarations/src';
+import { IOption } from './../components/Select/Select';
 
 export const Home = () => {
-  const [selectedColumn, setSelectedColumn] = useState('');
+  const [selectedColumn, setSelectedColumn] = useState<string>('');
 
-  const onSelectColumn = (column: string) => {
-    setSelectedColumn(column);
+  const onSelectColumn = (column:  SingleValue<IOption>) => {
+    if(column) {
+      setSelectedColumn(column.value);
+    }
   };
 
   return (
     <div className="container">
       <div className="select">
         <span>Искать по колонке: </span>
-        {' '}
       <Select onChange={onSelectColumn} />
       </div>
         <DataGrid
